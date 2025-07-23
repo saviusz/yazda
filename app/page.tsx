e69f9cfaffeb576ff17fdba0314292d02891ed3a
Seweryn Woliński <serweryn123@gmail.com>
2025-07-23 18:28:43 +0200

    feat: add sliders to dashboard
    Signed-off-by: Seweryn Woliński <serweryn123@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,18 @@ import clsx from "clsx";
 import style from "./page.module.css";
 import Button from "@/components/Button";
 import StreamPlayer from "@/components/StreamPlayer";
+import Slider from "@/components/Slider";
+import { useState } from "react";
 export default function Home() {
+
+  const [speed, setSpeed] = useState(0);
 
   return <main className={style.main}>
     <div className={clsx(style.column, style.left)}>
       Left
       <StreamPlayer />
+      <Slider label="Pozycja" value={90} progress={81.926}/>
+      <Slider label="Prędkość" value={speed} onChange={setSpeed} displayValue={`${Math.round(speed/100 * 20 * 2) / 2} kr/s`}/>
     </div>
     <div className={clsx(style.column, style.right)}>
       Right
